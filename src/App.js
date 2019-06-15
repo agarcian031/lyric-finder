d35468f5c488ea05c90079269dec2341e545a1fa
Amanda Garcia <algarcian@outlook.com>
2019-06-15 12:49:40 -0600

    Context API added and Tracks import

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {Provider} from './context';  
 import Navbar from './components/layout/Navbar'; 
-import Index from './components/layout/Index'; 
+import Index from './components/layout/Index';
 
 
 function App() {
   return (
-    <Router>
-    <>
-      <Navbar/>
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={Index}/>
-        </Switch>
-      </div>
-    </>
-    </Router>
+    <Provider>
+      <Router>
+      <>
+        <Navbar/>
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Index}/>
+          </Switch>
+        </div>
+      </>
+      </Router>
+    </Provider>
   );
 }
 
--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react'; 
+import {Consumer} from '../../context'; 
 
 class Tracks extends Component {
   render() {
     return (
-      <div>
-        <h1>Tracks</h1>
-      </div>
+      <Consumer>
+        { value => {
+          
+        }}
+      </Consumer>
     )
   }
 }
