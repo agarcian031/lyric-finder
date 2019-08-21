eb43723b2c385886e9fb3c848f77089b474f2599
Amanda Garcia <algarcian@outlook.com>
2019-08-21 10:35:06 -0500

    updated Readme.md

---
+++ precache-manifest.04bea3c3c60da8191046c5576997883e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "635f60cf7aa219e8730b8d188bb11099",
+    "url": "/lyric-finder/index.html"
+  },
+  {
+    "revision": "42d71babf88d322be2bf",
+    "url": "/lyric-finder/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "f207da0d6525e11aff33",
+    "url": "/lyric-finder/static/js/2.fbc50638.chunk.js"
+  },
+  {
+    "revision": "42d71babf88d322be2bf",
+    "url": "/lyric-finder/static/js/main.09be9962.chunk.js"
+  },
+  {
+    "revision": "481bd5097b36dc395d33",
+    "url": "/lyric-finder/static/js/runtime~main.3ec70798.js"
+  }
+]);
